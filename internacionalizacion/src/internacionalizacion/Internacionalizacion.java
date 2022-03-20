be11d43340730cdce6baa7a5d253a5a1cec69be4
JOTTV <99305833+JOTTV@users.noreply.github.com>
2022-03-20 15:43:55 -0500

    Add files via upload

---
+++ internacionalizacion/src/internacionalizacion/Internacionalizacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package internacionalizacion;
+
+import java.text.Normalizer.Form;
+
+/**
+ *
+ * @author 312C-27
+ */
+public class Internacionalizacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame f = new NewJFrame();
+        f.setVisible(true);
+        
+        
+    }
+    
+}
